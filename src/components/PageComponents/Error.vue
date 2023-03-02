f1667b1b14a3d5979aeaa6c1ce0f1da846beaff0
HarryWoodall <harry.woodall.gh@gmail.com>
2023-03-02 08:19:02 +0000

    style(error): update error page styling, add back button

--- src/components/PageComponents/Error.vue
+++ src/components/PageComponents/Error.vue
@@ -1,20 +1,27 @@
 <script lang="ts" setup>
+import StorageHelpers from "@/helpers/StorageHelper";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
+
+const backToLogin = () => {
+  router.removeRoute(router.currentRoute.value.name!);
+  StorageHelpers.DestroyLocalStorage();
+  router.push("/");
+};
 </script>
 
 <template>
   <v-container class="fill-height">
     <v-responsive class="d-flex align-center justify-center text-center fill-height">
-      <v-sheet elevation="4" color="rgba(0,0,0,0.25)" :rounded="'xl'" class="py-10 text-center mx-auto" max-width="500">
-        <h1 class="text-h2 font-weight-bold hidden-sm-and-down">Error</h1>
-        <h1 class="text-h3 font-weight-bold hidden-md-and-up">Error</h1>
+      <v-sheet elevation="4" color="rgba(0,0,0,0.25)" rounded="xl" class="pa-10 text-center mx-auto" max-width="500">
+        <h1 class="text-h4 font-weight-bold hidden-sm-and-down">Error</h1>
+        <h1 class="text-h5 font-weight-bold hidden-md-and-up">Error</h1>
 
-        <h2 class="text-h4">Something went wrong</h2>
-
-        <p class="text-body-1 font-weight-bold mt-10">{{ router.currentRoute.value.query.status }}</p>
+        <p class="text-h3 font-weight-bold mt-10">{{ router.currentRoute.value.query.status }}</p>
         <p class="text-overline font-weight-bold">{{ router.currentRoute.value.query.message }}</p>
+
+        <v-btn variant="tonal" class="mt-10" @click="backToLogin">Back to login</v-btn>
       </v-sheet>
     </v-responsive>
   </v-container>
